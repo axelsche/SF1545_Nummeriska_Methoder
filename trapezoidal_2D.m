e03f5e2c9c66b638ff3b5137328e2de9b11a451a
axelsche <143517947+axelsche@users.noreply.github.com>
2024-12-04 00:39:10 +0100

    Delete trapezoidal_2D.m

--- trapezoidal_2D.m
+++
@@ -1,7 +0,0 @@
-function I = trapezoidal_2D(f, R, n)
-    h = R / n;
-    r_values = linspace(0, R, n + 1);
-    g_values = f(r_values) .* r_values;
-    I = (h / 2) * (g_values(1) + 2 * sum(g_values(2:end-1)) + g_values(end));
-    I = 2 * pi * I; % Multiply by 2*pi for the polar integration
-end
